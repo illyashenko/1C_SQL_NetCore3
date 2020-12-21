20b998daccf0c64683b07d410dbf81c313b4bcab
Иван Илляшенко <illyashenko@inbox.ru>
2020-12-21 10:02:52 +0300

    new

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace ForOneCSQLAppNC3.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        // GET: About/<AboutController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+    }
+}
