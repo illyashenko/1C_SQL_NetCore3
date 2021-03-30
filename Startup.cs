1c6fdae18f846782d93ff318259eb2641db93227
illyashenko_iv <illyashenko_iv@OFFICE-IT-78>
2021-03-30 15:11:23 +0300

    #ev

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using ForOneCSQLAppNC3.Service;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ForOneCSQLAppNC3
 {
@@ -28,6 +22,7 @@ namespace ForOneCSQLAppNC3
             services.AddScoped<IServiceAPI, ServiceAPI>();
             services.AddScoped<IServiceSQL, ServiceSQL>();
             services.AddControllers();
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +33,12 @@ namespace ForOneCSQLAppNC3
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(el =>
+            {
+                el.SwaggerEndpoint("/swagger/v1/swagger.json", "1C API");
+            });
             app.UseRouting();
 
             app.UseAuthorization();
